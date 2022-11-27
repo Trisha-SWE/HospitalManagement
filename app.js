135f87939a7c6d6b4a7d9600275c484b81d8555f
Nafizul Islam <96583087+NafizNoyon@users.noreply.github.com>
2022-11-28 01:54:05 +0600

    Update app.js

--- app.js
+++ app.js
@@ -34,25 +34,10 @@ var receipt = require ('./controllers/receipt');
 const http = require('http');
 //var chat = require ('./controllers/chat');//
 
-var app = express();
-
-
-app.set('view engine ', 'ejs');
 
 
 
 
-app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
-app.use(cookie());
-//app.use(expressValidator());
-
-
-var server =app.listen(3000 , function(){
-
-    console.log('server started');
-});
 
 app.use('/login' ,login);
 app.use('/home' , home);
